70044ac8bfd77031a7398d1f1f6a09d0c82318e6
ManuelJM29 <ing.manueljimenezm@gmail.com>
2023-05-06 05:47:45 -0500

    Creacion de modelo de cita

--- models/appointment.go
+++ models/appointment.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Appointment struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id, omitempty"`
+	Title       string             `json:"tltle" bson:"title, omitempty"`
+	Description string             `json:"description" bson:"description, omitempty"`
+	StartDate   time.Time          `json:"start_date" bson:"start_date, omitempty"`
+	EndDate     time.Time          `json:"end_date" bson:"end_date, omitempty"`
+}
